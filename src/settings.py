9e9a76709940c9b6dda4f29e326bd0317a1ff443
Cristian Ticu <cristian.ticu@eggnita.com>
2025-03-16 19:29:20 +0200

    Add boto3

--- src/settings.py
+++ src/settings.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
+AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME')
+
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 POSTGRES_DB_NAME = os.environ.get('POSTGRES_DB_NAME')
 POSTGRES_ADDRESS = os.environ.get('POSTGRES_ADDRESS')
 POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-ENVIRONMENT = os.environ.get('ENVIRONMENT')
+
+DYNAMODB_URL_OVERRIDE = os.environ.get('DYNAMODB_URL_OVERRIDE')
